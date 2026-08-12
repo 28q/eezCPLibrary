a7bd8412f062d5e9ba73dcb6860e85cd828b0bf1
highlighter-math <140462663+highlighter-math@users.noreply.github.com>
2026-08-12 18:26:02 +0900

    Update UnionFind.hpp

--- data-structure/UnionFind.hpp
+++ data-structure/UnionFind.hpp
@@ -2,30 +2,54 @@
 
 struct UnionFind{
 	private:
-	int N;
-	vector<int> par;
-	int root(int x){
-		int x0=x;
-		while(par[x]>=0){
-			x=par[x];
-		}
-		while(par[x0]>=0){
-			int x1=par[x0];
-			par[x0]=x;
-			swap(x0,x1);
-		}
-		return x;
-	}
+	vector<int32_t> p;
 	public:
-	UnionFind(int siz) : N(siz),par(siz,-1){}
-	void merge(int a,int b){
-		a=root(a),b=root(b);
-		if(a==b) return;
-		if(-par[a]<-par[b]) swap(a,b);
-		par[a]+=par[b];
-		par[b]=a;
+	explicit UnionFind(int siz) : p(siz){
+		int32_t i=0;
+		for (;i+1<siz;i+=2){
+			const uint64_t v= uint64_t(uint32_t(i))|(uint64_t(uint32_t(i+1))<<32);
+			*reinterpret_cast<uint64_t*>(p.data()+i)=v;
+		}
+		if(i<siz) p[i]=i;
 	}
-	bool same(int a,int b){
-		return root(a)==root(b);
+	[[gnu::always_inline]]
+	inline void merge(int32_t x,int32_t y) noexcept{
+		int32_t* const __restrict__ pp=p.data();
+		int32_t px=pp[x];
+		int32_t py=pp[y];
+		while(px!=py)[[likely]]{
+			if(px<py)[[likely]]{
+				if(x==px){
+					pp[x]=py;
+					return;
+				}
+				pp[x]=py;
+				x=px;
+				px=pp[x];
+			}
+			else{
+				if (y == py){
+					pp[y]=px;
+					return;
+				}
+				pp[y] = px;
+				y=py;
+				py=pp[y];
+			}
+		}
+		return;
+	}
+	[[gnu::always_inline]]
+	inline bool same(int32_t x,int32_t y) noexcept{
+		int32_t* const __restrict__ pp=p.data();
+		while(pp[x]!=x){
+			pp[x]=pp[pp[x]];
+			x=pp[x];
+		}
+		while(pp[y]!=y){
+			pp[y]=pp[pp[y]];
+			y=pp[y];
+		}
+		return x==y;
 	}
 };
