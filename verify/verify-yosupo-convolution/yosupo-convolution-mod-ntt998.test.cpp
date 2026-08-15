2ff0c69851f201702cf75910164087e016e942ac
28q <79777472+28q@users.noreply.github.com>
2026-08-15 21:26:43 +0900

    Refactor Yosupo convolution test for better I/O handling

--- verify/verify-yosupo-convolution/yosupo-convolution-mod-ntt998.test.cpp
+++ verify/verify-yosupo-convolution/yosupo-convolution-mod-ntt998.test.cpp
@@ -1,52 +1,28 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/convolution_mod"
 
-#ifndef FASTIO_UNSAFE_BLOCK_LOG
-#define FASTIO_UNSAFE_BLOCK_LOG 12
-#endif
-
-#include <cstddef>
+#include <iostream>
 #include <vector>
 
 #include "../../convolution/ntt998.hpp"
 
-#if defined(__GNUC__) && !defined(__clang__) && \
-    (defined(__x86_64__) || defined(__i386__))
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
-#elif defined(__clang__) && \
-    (defined(__x86_64__) || defined(__i386__))
-#pragma clang attribute push( \
-    __attribute__((target("avx2,bmi,bmi2,lzcnt,popcnt,ssse3"))), \
-    apply_to = function)
-#endif
+int main(){
+    std::cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
 
-#include "../../IO/fastio_unsafe.hpp"
+    std::size_t n, m;
+    std::cin >> n >> m;
 
-int main() {
-    fastio_unsafe io;
-    char* input_cursor = io.input_cursor();
-    char* output_cursor = io.output_cursor();
-    char* const output_end = io.output_end();
+    using mint = eez::ntt998::mint;
+    std::vector<mint> a(n), b(m);
 
-    const std::size_t n = io.read_u32(input_cursor);
-    const std::size_t m = io.read_u32(input_cursor);
-    std::vector<eez::ntt998::mint> a(n), b(m);
+    for(auto& x : a) std::cin >> x;
+    for(auto& x : b) std::cin >> x;
 
-    for (auto& value : a) {
-        value = eez::ntt998::mint::raw(io.read_u32(input_cursor));
+    const auto c = eez::ntt998::convolution(a, b);
+
+    for(std::size_t i = 0; i < c.size(); ++i){
+        if(i) std::cout << ' ';
+        std::cout << c[i];
     }
-    for (auto& value : b) {
-        value = eez::ntt998::mint::raw(io.read_u32(input_cursor));
-    }
-
-    const auto result = eez::ntt998::convolution(a, b);
-    for (const auto value : result) {
-        io.write_u32(output_cursor, output_end, value.val());
-    }
-    io.finish(output_cursor);
+    std::cout << '\n';
 }
-
-#if defined(__clang__) && \
-    (defined(__x86_64__) || defined(__i386__))
-#pragma clang attribute pop
-#endif
