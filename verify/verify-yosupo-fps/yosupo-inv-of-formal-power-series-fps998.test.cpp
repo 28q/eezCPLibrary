f57281bec3eb89698f963e224a8dfc404c0b7bde
28q <79777472+28q@users.noreply.github.com>
2026-09-07 02:51:57 +0900

    Improve performance and refactor test for FPS998
    Refactor input/output handling and optimize with fastio_unsafe.

--- verify/verify-yosupo-fps/yosupo-inv-of-formal-power-series-fps998.test.cpp
+++ verify/verify-yosupo-fps/yosupo-inv-of-formal-power-series-fps998.test.cpp
@@ -1,27 +1,25 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/inv_of_formal_power_series"
 
-#include <iostream>
-#include <vector>
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 
-#include "../../fps/fps998.hpp"
+#include "IO/fastio_unsafe.hpp"
+#include "fps/fps998.hpp"
 
 int main(){
-    std::cin.tie(nullptr);
-    std::ios::sync_with_stdio(false);
-
-    std::size_t n;
-    std::cin>>n;
-
+    fastio_unsafe io;
+    char* in=io.in.cursor();
+    char* out=io.out.begin();
+    char* const out_end=io.out.end();
+    using u32=fastio_unsafe_impl::u32;
     using mint=eez::fps998::mint;
-
-    std::vector<mint> f(n);
-    for(auto& x:f) std::cin>>x;
-
-    const auto g=eez::fps998::inv(f);
-
-    for(std::size_t i=0;i<n;++i){
-        if(i) std::cout<<' ';
-        std::cout<<g[i];
-    }
-    std::cout<<'\n';
+    const u32 n=fastio_unsafe_impl::read_u32_lt1e9(in);
+    std::vector<mint> f;
+    f.reserve(n);
+    for(u32 i=0;i<n;++i) f.emplace_back(mint::raw(fastio_unsafe_impl::read_u32_lt1e9(in)));
+    std::vector<mint> g(n);
+    eez::fps998::workspace ws;
+    eez::fps998::inv_to(f,g,ws);
+    for(u32 i=0;i<n;++i) fastio_unsafe_impl::write_u32_lt1e9(io.out,out,out_end,g[i].get());
+    io.out.finish(out);
 }
