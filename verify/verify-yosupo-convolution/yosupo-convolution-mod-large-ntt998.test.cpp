f96c0bec5491bcf17a913b3f5b5376e7e56fc8f7
28q <79777472+28q@users.noreply.github.com>
2026-09-08 14:21:35 +0900

    Update yosupo-convolution-mod-large-ntt998.test.cpp

--- verify/verify-yosupo-convolution/yosupo-convolution-mod-large-ntt998.test.cpp
+++ verify/verify-yosupo-convolution/yosupo-convolution-mod-large-ntt998.test.cpp
@@ -4,9 +4,8 @@
 #define FASTIO_UNSAFE_BLOCK_LOG 12
 #endif
 
-#include <bit>
 #include <cstddef>
-#include <vector>
+#include <cstdint>
 
 #include "../../convolution/ntt998.hpp"
 
@@ -31,39 +30,22 @@ int main() {
 
     const std::size_t n = io.read_u32(input_cursor);
     const std::size_t m = io.read_u32(input_cursor);
-    const std::size_t result_size = n + m - 1;
-    const std::size_t transform_size = std::bit_ceil(result_size);
-    using mint = eez::ntt998::mint;
 
-    if (transform_size < 32) {
-        std::vector<mint> a(n), b(m);
-        for (auto& value : a) value = mint::raw(io.read_u32(input_cursor));
-        for (auto& value : b) value = mint::raw(io.read_u32(input_cursor));
-
-        const auto result = eez::ntt998::convolution(a, b);
-        for (const mint value : result) {
-            io.write_u32(output_cursor, output_end, value.val());
+    eez::ntt998::convolution_normal_io(
+        n, m,
+        [&]() -> std::uint32_t {
+            return io.read_u32_lt1e9(input_cursor);
+        },
+        [&](std::uint32_t value) {
+            io.write_u32_lt1e9(output_cursor, output_end, value);
         }
-    } else {
-        eez::ntt998::convolution_buffer a(transform_size), b(transform_size);
-        for (std::size_t i = 0; i < n; ++i) {
-            a[i] = mint::raw(io.read_u32(input_cursor));
-        }
-        for (std::size_t i = 0; i < m; ++i) {
-            b[i] = mint::raw(io.read_u32(input_cursor));
-        }
-
-        eez::ntt998::convolution_inplace(a, b);
-        for (std::size_t i = 0; i < result_size; ++i) {
-            io.write_u32(output_cursor, output_end, a[i].val());
-        }
-    }
+    );
 
     io.finish(output_cursor);
+    return 0;
 }
 
 #if defined(__clang__) && \
     (defined(__x86_64__) || defined(__i386__))
 #pragma clang attribute pop
 #endif
-
